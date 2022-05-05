84dfad2fd9aca6833f9642101b973fcaccdb11e4
Luiz Netto <luiz.netto@accenture.com>
2022-05-05 17:13:00 -0300

    Primeiro texto em uma tela em RN

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from "react";
+import {
+  Text,
+} from "react-native";
+
+  /*  =========== COMPONENTES FUNCIONAIS ==========
+  * Componentes funcionais retornam um JSX para ser renderizados.
+  * no corpo do código antes da função "return()" pode ser utilizado 
+  * TS/JS livremente, enquanto que para utiliza-los dentro da função
+  * de retorno se faz necessário o uso de chaves {}.
+  */
+
+const App = () => <Text>HOME</Text>
+
+export default App;
