97a69724f4233531bc98e62e72f0d7996a47718e
Luiz Netto <luiz.netto@accenture.com>
2022-05-06 15:04:47 -0300

    Propriedade dos componentes

--- src/App.tsx
+++ src/App.tsx
@@ -8,23 +8,100 @@
  * @format
  */
 
-import React from "react";
-import { SafeAreaView, Text } from "react-native";
+import React, { useState } from 'react';
+import { SafeAreaView, TextInput, View } from 'react-native';
 
-/*  =========== ESTILIZAÇÃO ==========
- *   Cada componente pode ter seu estilo customizado
- *   ao se passar uma proprieda para tal componente.
- *   A estilização mais simples é a estilizaçnao Inline,
- *   aonde o componente recebe, na mesma linha da declaração
- *   da propriedade "style", um objeto contendo todas as
- *   opções de estilo para o componente.
+const VALID_PASSWORD = 8;
+const MAX_CHARACTER_TEXT = 15;
+
+/*  =========== PROPRIEDADE DOS COMPONENTS ==========
+ *  Cada componente é generalizado e, portanto, pode receber diversas
+ *  propriedades que vão ajudar a customizar seu uso para cada situação.
+ *
+ *  Nessa tela queremos construir um funcionalidade simples de login
+ *  e para tal iremos customizar dois componentes TextInput.
+ *  Tanto os campos de input de texto apra senha e nome de usuário
+ *  estão com diversas propriedades que vão de acordo com aquilo
+ *  que você precisa para construir sua tela/aplicação.
  */
 
 const App = () => {
+  const [login, onChangeLogin] = useState('');
+  const [password, onChangePassword] = useState('');
+  const [validPassword, setValidPassword] = useState(false);
+
   return (
     <SafeAreaView
-      style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>HOME</Text>
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#000000',
+      }}>
+      <View
+        style={[
+          {
+            width: 250,
+            alignItems: 'flex-start',
+            borderBottomWidth: 1,
+            marginVertical: 10,
+          },
+          validPassword
+            ? { borderColor: '#55AA22' }
+            : { borderColor: '#FFFFFF' },
+        ]}>
+        <TextInput
+          placeholderTextColor="#55AA22"
+          placeholder="USERNAME"
+          keyboardType="email-address"
+          maxLength={MAX_CHARACTER_TEXT}
+          autoCapitalize="none"
+          autoComplete="username"
+          autoCorrect={false}
+          value={login}
+          onChangeText={onChangeLogin}
+          style={{
+            fontSize: 22,
+            marginTop: 5,
+            color: '#FFFFFF',
+          }}
+        />
+      </View>
+      <View
+        style={[
+          {
+            width: 250,
+            alignItems: 'flex-start',
+            borderBottomWidth: 1,
+            marginVertical: 10,
+          },
+          validPassword
+            ? { borderColor: '#55AA22' }
+            : { borderColor: '#FFFFFF' },
+        ]}>
+        <TextInput
+          placeholderTextColor="#55AA22"
+          placeholder="PASSWORD"
+          keyboardType="numeric"
+          maxLength={MAX_CHARACTER_TEXT}
+          autoCapitalize="none"
+          autoComplete="password"
+          secureTextEntry={true}
+          autoCorrect={false}
+          value={password}
+          onChangeText={newText => {
+            password.length >= VALID_PASSWORD
+              ? setValidPassword(true)
+              : setValidPassword(false);
+            onChangePassword(newText);
+          }}
+          style={{
+            fontSize: 22,
+            marginTop: 5,
+            color: '#FFFFFF',
+          }}
+        />
+      </View>
     </SafeAreaView>
   );
 };
